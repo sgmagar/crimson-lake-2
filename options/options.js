f0ab7addf56ef12760e61a227430b37b5a926ab7
Saurav <sp.gharti@gmail.com>
2023-03-29 16:09:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "crimson-lake-2",
+  url: "https://crimson-lake-2.botics.co",
+  api: "https://crimson-lake-2.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
